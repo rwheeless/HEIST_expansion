98abab0da66ae545e30a5a2b701f5274b38336b1
mlighta7 <lesali713@gmail.com>
2021-04-24 00:12:35 -0400

    Voiceline2and4added

---
+++ Project 2 - 3D Exploration/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public AudioSource voicelines;
+
+    public AudioClip voice2;
+    [SerializeField]
+    GameObject VoiceTrigger2;
+    private bool playVoice2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        voicelines = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (playVoice2 == true)
+        {
+            voicelines.clip = voice2;
+            voicelines.Play();
+            
+            VoiceTrigger2.transform.position = new Vector3(-6, 0.5f, 3);
+
+            playVoice2 = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("voice2"))
+        {
+            playVoice2 = true;
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playVoice2 = true;
+        }
+    }
+}
---
+++ Project 2 - 3D Exploration/Assets/Scripts/Teleporter2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter2 : MonoBehaviour
+{
+    public AudioSource voicelines;
+
+    public AudioClip voice4;
+    [SerializeField]
+    GameObject VoiceTrigger4;
+    private bool playVoice4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        voicelines = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (playVoice4 == true)
+        {
+            voicelines.clip = voice4;
+            voicelines.Play();
+            
+            VoiceTrigger4.transform.position = new Vector3(-60, 0.5f, 3);
+
+            playVoice4 = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("voice4"))
+        {
+            playVoice4 = true;
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playVoice4 = true;
+        }
+    }
+}
